6446387efc059c015aabeb30f23b426a8ded5792
ChrishaneA <76893502+ChrisA2021@users.noreply.github.com>
2023-05-27 20:35:25 +1000

    Project Created

---
+++ MyHabits/MyHabits/MyHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitsApp.swift
+//  MyHabits
+//
+//  Created by Chrishane Amarasekara on 27/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
